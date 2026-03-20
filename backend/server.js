503b6b783d7490aaf0afa14d3b8d31c73a221ee7
africaramotshela-alt <africaramotshela@gmail.com>
2026-03-20 17:24:20 +0000

    Back and frantend

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,44 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/donations', require('./routes/donations'));
 app.use('/api/messages', require('./routes/messages'));
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://africaramotshela_db_user:<X4UdeQ8-ra!4Yca>@cluster0.wyyfhyd.mongodb.net/?appName=Cluster0"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir)
+
+app.listen(...)
+
+app.get("/api/users/leaderboard", async (req, res) => {
+  try {
+    const users = await User.find().sort({ score: -1 });
+    res.json(users);
+  } catch (err) {
+    console.error("Leaderboard error:", err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> app.listen(process.env.PORT || 5000, ()=>console.log("Server running")))
 .catch(err=>console.log(err));
@@ -24,6 +62,7 @@ const hpp = require('hpp');
 app.use(helmet());
 app.use(xss());
 app.use(hpp());
+
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
